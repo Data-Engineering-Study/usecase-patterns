f4d1a110d2125d80dc1e9dc406aa5149a7e6e383
twosom <two_somang@icloud.com>
2025-02-18 12:19:41 +0900

    spotlessApply 플러그인 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,44 @@
 plugins {
-    id("java")
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.declub"
 version = "1.0-SNAPSHOT"
+
+allprojects {
+    plugins.apply("com.diffplug.spotless")
+    spotless {
+        java {
+            importOrder()
+            cleanthat()
+            removeUnusedImports()
+            googleJavaFormat()
+        }
+        kotlin {
+            ktfmt()
+            ktlint()
+        }
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint()
+        }
+        yaml {
+            target("src/**/*.yaml", "src/**/*.yml")
+            jackson()
+            prettier()
+        }
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
